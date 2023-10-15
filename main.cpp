ec602b3b89a9c0ed1fe3a4b38b6a535e8e781ceb
Robert Swanson <rswanson3141@gmail.com>
2023-10-15 00:15:46 -0500

    Fix CI

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace Common::Game;
 
 int main()
 {
-    Player player1 = {"A"};
-    Player player2 = {"B"};
-    Player player3 = {"C"};
-    Player player4 = {"D"};
+    Player player1 = Player{"A"};
+    Player player2 = Player{"B"};
+    Player player3 = Player{"C"};
+    Player player4 = Player{"D"};
     std::array<Player, Common::Constants::NUM_PLAYERS> players = {player1, player2, player3, player4};
     Game game({player1, player2, player3, player4});
     game.runGame();
