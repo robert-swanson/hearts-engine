2c2d6149ece5c6c676f259cefd6103f45b9c2021
Robert Swanson <rswanson3141@gmail.com>
2023-10-29 20:49:31 -0500

    Print version

--- server/constants.h
+++ server/constants.h
@@ -2,7 +2,7 @@
 
 namespace Common::Server
 {
-    constexpr int SERVER_PORT = 404040;
+    constexpr uint16_t SERVER_PORT = 40404;
     constexpr int MAX_CONNECTION_BACKLOG = 5;
 
     constexpr int SUCCESS_CODE = 0;
